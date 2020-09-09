0fd25bfaea62c51f75d97edfc78235099abcf809
Andrii Sokolovskyi <asoko3@softserveinc.com>
2020-09-09 16:33:51 +0300

    create components, retrieve data into information component, make use of devextreme datagrid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  {
+    path: 'info',
+    loadChildren: () => import('./components/information/information.module')
+      .then(m => m.InformationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/information/info.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+export interface Currency {
+  currencyExcerpt: string;
+  exchangeDate: string;
+  id?: number;
+  rate: number;
+  text: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoService {
+
+  constructor(private http: HttpClient) { }
+
+  getApi(): Observable<Currency[]> {
+    return this.http.get<Currency[]>('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+  }
+}
