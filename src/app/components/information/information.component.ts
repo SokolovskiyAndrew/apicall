42cf6ecf8174ea2a9650ae7485b13d56f9676097
Andrii Sokolovskyi <asoko3@softserveinc.com>
2020-09-09 19:07:07 +0300

    create pretier configurations

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {Currency, InfoService} from './info.service';
+import { Currency, InfoService } from './info.service';
 import { formatMessage } from 'devextreme/localization';
-
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.scss']
+  styleUrls: ['./information.component.scss'],
 })
 export class InformationComponent implements OnInit {
+  currencies: Currency[] = [];
 
-currencies: Currency[] = [];
-formatMessage = formatMessage;
-
-  constructor(private infoService: InfoService) { }
+  constructor(private infoService: InfoService) {}
 
   ngOnInit(): void {
-    this.infoService.getApi().subscribe(data => {
+    this.infoService.getApi().subscribe((data) => {
       this.currencies = data;
-      console.log(this.currencies)
     });
   }
-
 }
