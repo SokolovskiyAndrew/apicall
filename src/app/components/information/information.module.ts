a07a8fc613ee17ea3c0a5bc82cd8dd483b3370d7
Andrii Sokolovskyi <asoko3@softserveinc.com>
2020-09-10 10:52:01 +0300

    added custom preloader

--- src/app/components/information/information.module.ts
+++ src/app/components/information/information.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
-import {DxDataGridModule} from 'devextreme-angular';
+import {DxDataGridModule, DxLoadIndicatorModule} from 'devextreme-angular';
 
 import {InformationComponent} from './information.component';
 import {InformationRoutingModule} from './information-routing.module';
@@ -16,6 +16,7 @@ import {InfoService} from './info.service';
     CommonModule,
     InformationRoutingModule,
     HttpClientModule,
+    DxLoadIndicatorModule,
     DxDataGridModule
   ],
   providers: [
