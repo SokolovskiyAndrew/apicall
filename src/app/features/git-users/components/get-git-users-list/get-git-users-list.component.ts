a30ef12e8d6a07b1b5dc8bfb85544eaa89cb1fdd
Andrew Sokolovskyi <asoko3@softserveinc.com>
2020-10-07 15:49:15 +0300

    deleted useless variables

--- src/app/features/git-users/components/get-git-users-list/get-git-users-list.component.ts
+++ src/app/features/git-users/components/get-git-users-list/get-git-users-list.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {GetGitUsersDataService} from '../../services/get-git-users-data.service';
-import {GitUser} from '../../../../share-files/interfaces/git-user.interface';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-get-git-users-list',
@@ -9,17 +7,13 @@ import {Observable} from 'rxjs';
   styleUrls: ['./get-git-users-list.component.scss'],
 })
 export class GetGitUsersListComponent implements OnInit {
-  usersList$: Observable<GitUser[]>;
-  gitUsersArray: GitUser[] = [];
 
   constructor(private getUsersListService: GetGitUsersDataService) {}
 
   ngOnInit(): void {}
 
   getUserData(): void {
-    this.usersList$ = this.getUsersListService.getGitUsersData();
-    this.usersList$.subscribe((users) => {
-      this.gitUsersArray = users;
-    });
+    this.getUsersListService.getGitUsersData()
+    .subscribe();
   }
 }
